1ea3625a49fdd068e76a3723d24b9dfb26c664b4
rapospectre <rapospectre@163.com>
2017-06-24 16:08:56 +0800

    <feat>  add search in admin
    Author:rapospectre@gmail.com
    
    Desc: add search in admin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from core.models import *
+
+
 # Register your models here.
+
+class ColumnAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'slug', 'hash']
+
+
+class RandomColumnAdmin(admin.ModelAdmin):
+    search_fields = ['slug', 'hash']
+
 
 admin.site.register(Proxy)
 admin.site.register(ZHArticle)
 admin.site.register(ZHUser)
-admin.site.register(ZHColumn)
+admin.site.register(ZHColumn, ColumnAdmin)
 admin.site.register(Tag)
-admin.site.register(ZHRandomColumn)
+admin.site.register(ZHRandomColumn, RandomColumnAdmin)
