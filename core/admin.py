c6e9c72d16a8482855b74e9fb0738b770fd1f7e1
rapospectre <rapospectre@163.com>
2017-06-26 09:46:54 +0800

    <feat>  add search field in user
    Author:rapospectre@gmail.com
    
    Change-Id: I7a1169dccf014a702e8a2235656e5aabe65a4266
    Desc: add search field in user

--- core/admin.py
+++ core/admin.py
@@ -12,9 +12,13 @@ class RandomColumnAdmin(admin.ModelAdmin):
     search_fields = ['slug', 'hash']
 
 
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['slug', 'hash']
+
+
 admin.site.register(Proxy)
 admin.site.register(ZHArticle)
-admin.site.register(ZHUser)
+admin.site.register(ZHUser, UserAdmin)
 admin.site.register(ZHColumn, ColumnAdmin)
 admin.site.register(Tag)
 admin.site.register(ZHRandomColumn, RandomColumnAdmin)
