e91586357a05592ebe6dda592ea65ef77212e1bf
rapospectre <rapospectre@163.com>
2017-06-29 23:17:09 +0800

    <feat>  add style in article
    Author:rapospectre@gmail.com
    
    Change-Id: I26070369834f63b89541259ce517de1826c463ef
    Desc: add style in article

--- Lighthouse/settings.py
+++ Lighthouse/settings.py
@@ -109,3 +109,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = './static/'
+
+STATIC_MEDIA = './static/'
--- Lighthouse/urls.py
+++ Lighthouse/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import settings
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = [
 
     url(r'^', include('page.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^s/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_MEDIA}),
 ]
