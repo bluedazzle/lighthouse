8b45086bb34a03d159d35ed40e174e5eb3f03c2f
rapospectre <rapospectre@163.com>
2017-07-06 23:41:24 +0800

    <feat>  fix mobile style
    Author:rapospectre@gmail.com
    
    Change-Id: I12719276d679a33eb5e360d0dfd276f8cd877880
    Desc: fix mobile style

--- page/utils.py
+++ page/utils.py
@@ -11,11 +11,16 @@ SIZE_RAW = 'r'
 
 
 def convert_image_size(url, size=SIZE_BIG):
+    if 'default' in url:
+        return url
     try:
         url_list = url.split('.')
         image = '.'.join(url_list[:-1])
         ext = url_list[-1]
-        new_url = '{0}{1}.{2}'.format(image[:-1], size, ext)
+        if '_' not in image:
+            new_url = '{0}_{1}.{2}'.format(image, size, ext)
+        else:
+            new_url = '{0}{1}.{2}'.format(image[:-1], size, ext)
         return new_url
     except Exception as e:
         logging.exception('Convert image size error, url: {0} reason: {1}'.format(url, e))
