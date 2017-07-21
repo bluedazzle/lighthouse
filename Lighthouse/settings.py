3e51bd3dbbf7f409d575d21b887d3d494ff5e3bb
rapospectre <rapospectre@163.com>
2017-07-21 23:34:04 +0800

    add gzip

--- Lighthouse/settings.py
+++ Lighthouse/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
