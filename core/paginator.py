d1fb7525542bd16083cd0ebb0dfed8fd06c256fd
rapospectre <rapospectre@163.com>
2017-07-30 20:16:25 +0800

    <feat>  opt sql
    Author:rapospectre@gmail.com
    
    Change-Id: Ib1e0744e632df4574a10cfb1bb5ee7891584c0ce
    Desc: opt sql

--- core/paginator.py
+++ core/paginator.py
@@ -14,21 +14,35 @@ class RedisCachePaginator(Paginator):
         Returns the total number of objects, across all pages.
         """
         if self._count is None:
-            # count = cache.get('article_count')
-            # if count:
-            #     self._count = count
-            #     return self._count
+            count = cache.get('article_count')
+            if count:
+                self._count = count
+                return self._count
             try:
                 self._count = self.object_list.count()
             except (AttributeError, TypeError):
                 # AttributeError if object_list has no count() method.
                 # TypeError if object_list.count() requires arguments
                 # (i.e. is of type list).
-                self._count = len(list(self.object_list))
+                self._count = len(self.object_list)
             cache.set('article_count', self._count, 60 * 60 * 6)
         return self._count
 
     count = property(_get_count)
+
+    def page(self, number):
+        from core.models import ZHArticle, ZHColumn
+
+        """
+        Returns a Page object for the given 1-based page number.
+        """
+        number = self.validate_number(number)
+        bottom = (number - 1) * self.per_page
+        top = bottom + self.per_page
+        if top + self.orphans >= self.count:
+            top = self.count
+        self.object_list = ZHArticle.objects.raw('{0} OFFSET {1} LIMIT {2}'.format(self.object_list.raw_query, bottom, self.per_page))
+        return self._get_page(self.object_list, number, self)
 
 
 class SearchPaginator(Paginator):
