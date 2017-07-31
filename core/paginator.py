12706b63f89d783442fdad74cc3592aea856f0d8
rapospectre <rapospectre@163.com>
2017-07-31 13:00:03 +0800

    <fix>  cout
    Author:rapospectre@gmail.com
    
    Change-Id: I0ef407120952339fe52226b71731866212c6ce6e
    Desc: cout

--- core/paginator.py
+++ core/paginator.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.core.paginator import Paginator
 from django.core.cache import cache
+from sqlalchemy import func
 
 from lg_data.db.models import DBSession, ZHArticle
 from lg_data.utils import md5
