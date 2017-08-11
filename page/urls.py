196e6c34d0ee0f276eb9e4f1f2955b873c4545d0
rapospectre <rapospectre@163.com>
2017-08-11 18:58:38 +0800

    <fix>  fix column name
    Author:rapospectre@gmail.com
    
    Change-Id: Ic1a784f2707caa448ba41be978d65406189c8983
    Desc: fix column name

--- page/urls.py
+++ page/urls.py
@@ -4,7 +4,7 @@ from page.views import *
 urlpatterns = [
     # Examples:
     url(r'^article/(?P<slug>([a-z0-9-])+)/$', ArticleDetailView.as_view(), name='article'),
-    url(r'^column/(?P<slug>([a-z0-9-_])+)/$', ColumnDetailView.as_view(), name='column'),
+    url(r'^column/(?P<slug>([a-zA-Z0-9-_])+)/$', ColumnDetailView.as_view(), name='column'),
     url(r'^articles/$', ArticleListView.as_view(), name='articles'),
     url(r'^$', ArticleListView.as_view()),
 
